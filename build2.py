1b4face2ebf3caceb2480310cf718b4ceb4da64b
TuNombre <tuemail@gmail.com>
2026-05-20 17:55:02 -0500

    Actualizacion

--- build2.py
+++ build2.py
@@ -5,7 +5,7 @@ import streamlit as st
 # =========================================================
 
 st.set_page_config(
-    page_title="🇪🇨 OPTIMIZADOR RPG 🇪🇨 ",
+    page_title="🇪🇨 OPTIMIZAR BUILD 🇪🇨 ",
     layout="centered"
 )
 
